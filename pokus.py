ae931efca6895bfb6a3bfaa5f14469ab4c031096
jirinachlinger <nachlj01@jcu.cz>
2024-10-21 20:52:36 +0000

    #

--- pokus.py
+++ pokus.py
@@ -23,7 +23,30 @@
     #jmeno = input("zadej jmeno: ")
     #main(jmeno)
     #prijmeni = input("Zadej prijmeni")
-    #main(prijmeni)
+
+
+cislo = int(input("Zadej číslo: "))
+
+if int(cislo) <= 1:
+        #return False
+        print("False")
+else:
+    for i in range(2, int(cislo**0.5) + 1):
+        if cislo % i == 0:
+            #return False
+            print("False")
+            break
+    else:
+        #return True
+        print("True")
 
 
 
+#def vrat_prvocisla(maximum):
+maximum = cislo
+vrat_prvocisla = (maximum)
+seznam_prvocisel = []
+for n in range(2, maximum + 1):
+    if int(cislo(n)):
+        seznam_prvocisel.append(n)
+    print(seznam_prvocisel)
